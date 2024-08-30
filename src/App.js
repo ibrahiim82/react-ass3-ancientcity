d0bb62c0d202ad31d3028e837fa777dacc191e1e
ibrahiim82 <yavuzyolcu82@gmail.com>
2024-08-30 13:47:34 +0300

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import { Container } from "react-bootstrap";
+
+function App() {
+  // return <div className="container">Hello React</div>;
+  return <Container>Hello REACT</Container>;
+}
+
+export default App;
